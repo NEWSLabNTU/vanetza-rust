5b0e54956f1f8112ff6ec76a5dc756581b8cd056
aeon <jerry73204@gmail.com>
2024-03-02 13:16:06 +0800

    Add ItsAid type

--- vanetza-rust/src/common.rs
+++ vanetza-rust/src/common.rs
@@ -1,5 +1,29 @@
+mod its_aid;
 mod manual_runtime;
 mod runtime;
 
+pub use its_aid::*;
 pub use manual_runtime::*;
 pub use runtime::*;
+
+pub mod aid {
+    use super::ItsAid;
+
+    pub const CA: ItsAid = 36;
+    pub const DEN: ItsAid = 37;
+    pub const TLM: ItsAid = 137;
+    pub const RLT: ItsAid = 138;
+    pub const IVI: ItsAid = 139;
+    pub const TLC_R: ItsAid = 140;
+    pub const TLC_S: ItsAid = 637;
+    pub const GN_MGMT: ItsAid = 141;
+    pub const CRL: ItsAid = 622;
+    pub const SCR: ItsAid = 623;
+    pub const CTL: ItsAid = 624;
+    pub const VRU: ItsAid = 638;
+    pub const CP: ItsAid = 639;
+    pub const IMZ: ItsAid = 640;
+    pub const SA: ItsAid = 540801;
+    pub const GPC: ItsAid = 540802;
+    pub const IPV6_ROUTING: ItsAid = 270549118;
+}
---
+++ vanetza-rust/src/common/its_aid.rs
@@ -0,0 +1,2 @@
+pub type ItsAid = u32;
+
