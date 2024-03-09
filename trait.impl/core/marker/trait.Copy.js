2050b686fce083b70e866f8178f0f63f39c7d85d
jerry73204 <jerry73204@users.noreply.github.com>
2024-03-09 04:35:48 +0000

    deploy: a41344524e168285742cf59e4f22e3099c41e833

---
+++ trait.impl/core/marker/trait.Copy.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vanetza":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vanetza/units/struct.TrueNorth.html\" title=\"struct vanetza::units::TrueNorth\">TrueNorth</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/marker/trait.Copy.html\" title=\"trait core::marker::Copy\">Copy</a> for <a class=\"struct\" href=\"vanetza/units/struct.GeoAngle.html\" title=\"struct vanetza::units::GeoAngle\">GeoAngle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/vanetza/geonet/data_request/trait.DataRequestWithAddress.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"vanetza":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
