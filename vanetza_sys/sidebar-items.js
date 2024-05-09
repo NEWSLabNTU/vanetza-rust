423a34b2f2720f27b1315c0c3b6e8aa484ebb65f
Aesop-programmer <Aesop-programmer@users.noreply.github.com>
2024-05-09 11:34:15 +0000

    deploy: 125641065192fdb5e928760041a444d5d8966cb7

--- vanetza_sys/sidebar-items.js
+++ vanetza_sys/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["btpb_write","make_string"],"mod":["vanetza","vanetza_wrapper"],"struct":["std_initializer_list_uint8_t_AutocxxConcrete"],"trait":["ToCppString"],"type":["uint_fast32_t"]};
+window.SIDEBAR_ITEMS = {"fn":["btpb_read","btpb_write","make_string"],"mod":["vanetza","vanetza_wrapper"],"struct":["std_initializer_list_uint8_t_AutocxxConcrete"],"trait":["ToCppString"],"type":["uint_fast32_t"]};
