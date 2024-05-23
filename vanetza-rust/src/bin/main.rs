02679153a3accaa147074f4b997efdcfb8eb2297
Aesop-programmer <abc091044466@gmail.com>
2024-05-23 12:03:18 +0800

    update main

--- vanetza-rust/src/bin/main.rs
+++ vanetza-rust/src/bin/main.rs
@@ -5,7 +5,6 @@ use cxx::let_cxx_string;
 use std::pin::Pin;
 fn main(){
     let_cxx_string!(buf = "");
-    //let buf_ref = buf.as_ref();
-    let a = btpb_read("lo",buf.as_mut());
-    println!("{:?}",buf);
+    btpb_read("lo",buf.as_mut());
+    btpb_write("lo",b"newslab".as_slice());
 }
